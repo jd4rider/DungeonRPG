0e159b83445be6f45bd00e1fa1f918e30c471ef7
Jonathan Forrider <jd4rider@gmail.com>
2024-03-09 00:51:12 -0700

    Overriding Methods

--- Scripts/Characters/Players/Player.cs
+++ Scripts/Characters/Players/Player.cs
@@ -3,4 +3,18 @@ using System;
 
 public partial class Player : CharacterBody3D
 {
+    public override void _PhysicsProcess(double delta)
+    {
+        
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        if (@event.IsActionPressed("move_forward")) {
+            GD.Print("move forward");
+        }
+        GD.Print("Player Input");
+    }
+
+
 }
