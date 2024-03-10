b43d134bfefdff7c7aaeae89b2e9fbd4587c3b36
Jonathan Forrider <jd4rider@gmail.com>
2024-03-10 00:06:07 -0700

    Building the State Machine

--- Scripts/Characters/Players/PlayerIdleState.cs
+++ Scripts/Characters/Players/PlayerIdleState.cs
@@ -9,4 +9,15 @@ public partial class PlayerIdleState : Node
         Player characterNode = GetOwner<Player>();
         characterNode.animPlayerNode.Play(ANIM_IDLE);
     }
+
+    public override void _Notification(int what)
+    {
+        base._Notification(what);
+
+        if (what == 5001)
+        {
+            Player characterNode = GetOwner<Player>();
+            characterNode.animPlayerNode.Play(ANIM_IDLE);
+        }
+    }
 }
