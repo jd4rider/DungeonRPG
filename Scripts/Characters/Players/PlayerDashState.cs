b2d3c157a6f09d909a35f81fc8c6ea08714a6e1b
Jonathan Forrider <jd4rider@gmail.com>
2024-03-10 01:31:46 -0700

    Setting Up the Dash State

---
+++ Scripts/Characters/Players/PlayerDashState.cs
@@ -0,0 +1,27 @@
+using Godot;
+using System;
+using static GameConstants;
+
+public partial class PlayerDashState : Node
+{
+    private Player characterNode;
+
+    public override void _Ready()
+    {
+        characterNode = GetOwner<Player>();
+        SetPhysicsProcess(false);
+    }
+
+    public override void _Notification(int what)
+    {
+        base._Notification(what);
+
+        if (what == 5001)
+        {
+            characterNode.animPlayerNode.Play(ANIM_DASH);
+            SetPhysicsProcess(true);
+        }
+        else if (what == 5002) SetPhysicsProcess(false);
+    }
+}
+
--- Scripts/General/GameConstants.cs
+++ Scripts/General/GameConstants.cs
@@ -1,10 +1,12 @@
-public class GameConstants {
+public class GameConstants
+{
     public const string ANIM_IDLE = "Idle";
     public const string ANIM_MOVE = "Move";
+    public const string ANIM_DASH = "Dash";
 
     public const string INPUT_MOVE_LEFT = "MoveLeft";
     public const string INPUT_MOVE_RIGHT = "MoveRight";
     public const string INPUT_MOVE_FORWARD = "MoveForward";
     public const string INPUT_MOVE_BACKWARD = "MoveBackward";
-    
+
 }
