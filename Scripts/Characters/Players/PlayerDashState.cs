d583cd5625df824d03c56ccc5ba2a6289151ff79
Jonathan Forrider <jd4rider@gmail.com>
2024-03-10 03:31:31 -0600

    Finalizing the Dash State

--- Scripts/Characters/Players/PlayerDashState.cs
+++ Scripts/Characters/Players/PlayerDashState.cs
@@ -6,12 +6,20 @@ public partial class PlayerDashState : Node
 {
     private Player characterNode;
     [Export] private Timer dashTimerNode;
+    [Export] private float speed = 10;
 
     public override void _Ready()
     {
         characterNode = GetOwner<Player>();
         dashTimerNode.Timeout += HandleDashTimeout;
         SetPhysicsProcess(false);
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        characterNode.MoveAndSlide();
+
+        characterNode.Flip();
     }
 
     public override void _Notification(int what)
@@ -21,6 +29,19 @@ public partial class PlayerDashState : Node
         if (what == 5001)
         {
             characterNode.animPlayerNode.Play(ANIM_DASH);
+            characterNode.Velocity = new(
+                characterNode.direction.X, 0, characterNode.direction.Y
+            );
+
+
+            if (characterNode.Velocity == Vector3.Zero)
+            {
+                characterNode.Velocity = characterNode.spriteNode.FlipH ?
+                  Vector3.Left :
+                  Vector3.Right;
+            }
+
+            characterNode.Velocity *= speed;
             dashTimerNode.Start();
             SetPhysicsProcess(true);
         }
@@ -29,6 +50,7 @@ public partial class PlayerDashState : Node
 
     private void HandleDashTimeout()
     {
+        characterNode.Velocity = Vector3.Zero;
         characterNode.stateMachineNode.SwitchState<PlayerIdleState>();
     }
 }
